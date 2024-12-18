00a38305a4b836de75429c00f263df8e7b4f232d
CoderAnonymos <adepojumayomikun1@gmail.com>
2024-12-18 17:57:04 +0000

    Commiting it for change

---
+++ Tuple to list.py
@@ -0,0 +1,8 @@
+# Tuple containing personal information
+personal_info = ("Mayomikun", "Adepoju", 10, "5ft", "35kg", "Music")
+
+# Converting the tuple to a list
+personal_info_list = list(personal_info)
+
+# Printing the list
+print(personal_info_list)
